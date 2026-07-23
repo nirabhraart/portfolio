43dc1c71cdef28b38d98c2cea029abafc1b21e05
nirabhraart <nirabhra27@gmail.com>
2026-07-23 14:22:36 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,15 +58,15 @@
 			on = function() {
 
 				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
+					//.removeClass('fixed')
+					//.css('transform', 'matrix(1,0,0,1,0,0)');
 
 				$window
 					.on('scroll._parallax', function() {
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+						//$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
 					});
 
@@ -76,7 +76,7 @@
 
 				$bg
 					.addClass('fixed')
-					.css('transform', 'none');
+					//.css('transform', 'none');
 
 				$window
 					.off('scroll._parallax');
